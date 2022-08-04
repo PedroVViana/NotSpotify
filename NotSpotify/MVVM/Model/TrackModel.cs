e2320ab8e06372ca6d194d64b132a4e71d9c5f21
Vanlume Pedro <ext.pedro.vanlume@o-pitblast.com>
2022-08-04 08:37:29 -0300

    Commit

---
+++ NotSpotify/MVVM/Model/TrackModel.cs
@@ -0,0 +1,267 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotSpotify.MVVM.Model
+{
+    public partial class TrackModel
+    {
+        [JsonProperty("albums")]
+        public Albums Albums { get; set; }
+    }
+
+    public partial class Albums
+    {
+        [JsonProperty("href")]
+        public Uri Href { get; set; }
+
+        [JsonProperty("items")]
+        public Item[] Items { get; set; }
+
+        [JsonProperty("limit")]
+        public long Limit { get; set; }
+
+        [JsonProperty("next")]
+        public Uri Next { get; set; }
+
+        [JsonProperty("offset")]
+        public long Offset { get; set; }
+
+        [JsonProperty("previous")]
+        public Uri Previous { get; set; }
+
+        [JsonProperty("total")]
+        public long Total { get; set; }
+    }
+
+    public partial class Item
+    {
+        [JsonProperty("album_type")]
+        public AlbumTypeEnum AlbumType { get; set; }
+
+        [JsonProperty("artists")]
+        public Artist[] Artists { get; set; }
+
+        [JsonProperty("available_markets")]
+        public string[] AvailableMarkets { get; set; }
+
+        [JsonProperty("external_urls")]
+        public ExternalUrls ExternalUrls { get; set; }
+
+        [JsonProperty("href")]
+        public Uri Href { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("images")]
+        public Image[] Images { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("release_date")]
+        public DateTimeOffset ReleaseDate { get; set; }
+
+        [JsonProperty("release_date_precision")]
+        public ReleaseDatePrecision ReleaseDatePrecision { get; set; }
+
+        [JsonProperty("total_tracks")]
+        public long TotalTracks { get; set; }
+
+        [JsonProperty("type")]
+        public AlbumTypeEnum Type { get; set; }
+
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+
+        public string Duration { get; set; }
+    }
+
+    public partial class Artist
+    {
+        [JsonProperty("external_urls")]
+        public ExternalUrls ExternalUrls { get; set; }
+
+        [JsonProperty("href")]
+        public Uri Href { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public ArtistType Type { get; set; }
+
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+    }
+
+    public partial class ExternalUrls
+    {
+        [JsonProperty("spotify")]
+        public Uri Spotify { get; set; }
+    }
+
+    public partial class Image
+    {
+        [JsonProperty("height")]
+        public long Height { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+
+        [JsonProperty("width")]
+        public long Width { get; set; }
+    }
+
+    public enum AlbumTypeEnum { Album, Single };
+
+    public enum ArtistType { Artist };
+
+    public enum ReleaseDatePrecision { Day };
+
+    public partial class TrackModel
+    {
+        public static TrackModel FromJson(string json) => JsonConvert.DeserializeObject<TrackModel>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this TrackModel self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                AlbumTypeEnumConverter.Singleton,
+                ArtistTypeConverter.Singleton,
+                ReleaseDatePrecisionConverter.Singleton,
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class AlbumTypeEnumConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(AlbumTypeEnum) || t == typeof(AlbumTypeEnum?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "album":
+                    return AlbumTypeEnum.Album;
+                case "single":
+                    return AlbumTypeEnum.Single;
+            }
+            throw new Exception("Cannot unmarshal type AlbumTypeEnum");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (AlbumTypeEnum)untypedValue;
+            switch (value)
+            {
+                case AlbumTypeEnum.Album:
+                    serializer.Serialize(writer, "album");
+                    return;
+                case AlbumTypeEnum.Single:
+                    serializer.Serialize(writer, "single");
+                    return;
+            }
+            throw new Exception("Cannot marshal type AlbumTypeEnum");
+        }
+
+        public static readonly AlbumTypeEnumConverter Singleton = new AlbumTypeEnumConverter();
+    }
+
+    internal class ArtistTypeConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(ArtistType) || t == typeof(ArtistType?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            if (value == "artist")
+            {
+                return ArtistType.Artist;
+            }
+            throw new Exception("Cannot unmarshal type ArtistType");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (ArtistType)untypedValue;
+            if (value == ArtistType.Artist)
+            {
+                serializer.Serialize(writer, "artist");
+                return;
+            }
+            throw new Exception("Cannot marshal type ArtistType");
+        }
+
+        public static readonly ArtistTypeConverter Singleton = new ArtistTypeConverter();
+    }
+
+    internal class ReleaseDatePrecisionConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(ReleaseDatePrecision) || t == typeof(ReleaseDatePrecision?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            if (value == "day")
+            {
+                return ReleaseDatePrecision.Day;
+            }
+            throw new Exception("Cannot unmarshal type ReleaseDatePrecision");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (ReleaseDatePrecision)untypedValue;
+            if (value == ReleaseDatePrecision.Day)
+            {
+                serializer.Serialize(writer, "day");
+                return;
+            }
+            throw new Exception("Cannot marshal type ReleaseDatePrecision");
+        }
+
+        public static readonly ReleaseDatePrecisionConverter Singleton = new ReleaseDatePrecisionConverter();
+    }
+}
+
+
---
+++ NotSpotify/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using NotSpotify.MVVM.Model;
+using RestSharp;
+using RestSharp.Authenticators.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotSpotify.MVVM.ViewModel
+{
+    
+    internal class MainViewModel
+    {
+
+        public ObservableCollection<Item> Songs { get; set; }
+        public MainViewModel()
+        {
+            Songs = new ObservableCollection<Item>();
+            PupolateCollection();
+        }
+
+        void PupolateCollection()
+        {
+            var client = new RestClient();
+            client.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator("BQA8RzdSvLMTf8Ap_nkXqsHp91N1Ctl2YUuNvPCjQ3lM518sxSb87GgrkaJyBZcm8jS_eLspMcq4c_HwdaMT7vFcrikH_jLgDKvQaEwUq1h6sXtlo62qKzaRt8zCRQThXyb8VV6zTgJ5-sokIl7nnKx-CpwuN8oAc7rVrn7UxiErwviUAaVMZ5C-8I_OGD62hSw", "Bearer");
+
+            var request = new RestRequest("https://api.spotify.com/v1/browse/new-releases", Method.Get);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+
+
+            var response = client.GetAsync(request).GetAwaiter().GetResult();
+            var data = JsonConvert.DeserializeObject<TrackModel>(response.Content);
+
+            for (int i = 0; i < data.Albums.Limit; i++)
+            {
+                var track = data.Albums.Items[i];
+               track.Duration = "2:32";
+               Songs.Add(track);
+                
+            }
+        }
+    }
+}
